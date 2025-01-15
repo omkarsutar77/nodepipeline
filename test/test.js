95d6985e4637850609cf1a7db65c09085acc2811
omkarsutar <sutaromkar911@gmail.com>
2025-01-15 23:16:47 +0530

    old

--- test/test.js
+++ test/test.js
@@ -2,15 +2,9 @@ var request = require('supertest');
 var app = require('../index.js');
  
 describe('GET /', function() {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
   it('respond with 404 page not found', function(done) {
-=======
-  it('respond with  parat change jhal ahe kay', function(done) {
->>>>>>> 445f293d64d975c66a5960db737b4ad8403cf932
-=======
-  it('respond with 404 page not found', function(done) {
->>>>>>> bc86b8d53a31836ae8f463975296388836b0f3a6
     request(app)
       .get('/nonexistentpage')
       .expect(404)
